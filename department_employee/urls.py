404495dcf7c94f371d439529a8bb70470e6fb2ae
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-23 19:36:45 +0400

    изменил url  employee_list для лучшего понимания назначения

--- department_employee/urls.py
+++ department_employee/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from department_employee import views
-from department_employee.views import DepartmentChainDetailView
+from department_employee.views import DepartmentDetailView
 
 urlpatterns = [
     path('', views.department_chain, name='department_chain'),
-    path('user-list/', views.employee_list, name='user_list'),
-    path('department_detail/<slug:slug>/', DepartmentChainDetailView.as_view(), name='department_detail')
+    path('employee_list/', views.employee_list, name='employee_list'),
+    path('department_detail/<slug:slug>/', DepartmentDetailView.as_view(), name='department_detail')
 
 ]
